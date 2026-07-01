5d20fedacb8b0b0450413d958e750411cf40a5f5
Thiago Gomez <thiagoagustingomez.2006+dev@gmail.com>
2026-07-01 15:49:30 -0300

    Bashrc, nvim and tmux

---
+++ config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- Use the system clipboard
+vim.opt.clipboard = "unnamedplus"
+-- Enable line numbers
+vim.opt.number = true
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.tabstop = 4           -- A tab character is displayed as 4 spaces
+vim.opt.shiftwidth = 4        -- Indent/outdent by 4 spaces
+vim.opt.softtabstop = 4       -- Pressing Tab inserts/removes 4 spaces
+vim.opt.smartindent = true    -- Optional: smart auto-indenting
---
+++ horizon/hypr/looknfeel.lua
@@ -0,0 +1,8 @@
+hl.config({
+    general = {
+        border_size = 10,
+        gaps_in = 5,
+        gaps_out = 15
+    }
+)
+
